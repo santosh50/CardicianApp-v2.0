a0eca42e3cca2f774d82acf1df114ff45d94d92a
Santosh Vasisht <svasisht@redhat.com>
2024-08-19 21:03:36 +0530

    Empty project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:cardician_app_v2/pages/homepage.dart';
+
+void main() {
+  //Ensure app is initialized before locking device orientation
+  WidgetsFlutterBinding.ensureInitialized(); //returns Future object
+
+  //Fix orientation to portrait and run the app
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(MaterialApp(
+            title: 'Cardician',
+            initialRoute: '/',
+            routes: {
+              '/': (context) => const HomePage(),
+            },
+          )));
+
+  runApp();
+}
