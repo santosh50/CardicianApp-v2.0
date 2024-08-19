2b984c21dfad85d1b1fd1c151030da8ad0b4b695
Santosh Vasisht <svasisht@redhat.com>
2024-08-19 22:15:10 +0530

    Add external packages

---
+++ lib/card_class.dart
@@ -0,0 +1,33 @@
+import 'package:playing_cards/playing_cards.dart';
+
+class MyCard {
+  Suit suit = Suit.joker;
+  CardValue value = CardValue.joker_1;
+  bool showBack = true;
+}
+
+final Map valueMap = {
+  CardValue.ace: 'A',
+  CardValue.two: '2',
+  CardValue.three: '3',
+  CardValue.four: '4',
+  CardValue.five: '5',
+  CardValue.six: '6',
+  CardValue.seven: '7',
+  CardValue.eight: '8',
+  CardValue.nine: '9',
+  CardValue.ten: '10',
+  CardValue.jack: 'J',
+  CardValue.queen: 'Q',
+  CardValue.king: 'K'
+};
+
+final Map suitMap = {
+  Suit.clubs: 'club',
+  Suit.hearts: 'heart',
+  Suit.spades: 'spade',
+  Suit.diamonds: 'diamond'
+};
+
+final List valList = valueMap.keys.toList();
+final List suitList = suitMap.keys.toList();
