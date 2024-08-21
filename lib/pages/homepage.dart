e8b9eb1cb960bf197ee9c80b649ddb3129edd7f2
Santosh Vasisht <svasisht@redhat.com>
2024-08-21 20:49:52 +0530

    Get flip_card package

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,5 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
