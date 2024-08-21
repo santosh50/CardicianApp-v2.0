da2742ae2b5ec39b61740644a40a6798d9f224e5
Santosh Vasisht <svasisht@redhat.com>
2024-08-21 20:41:25 +0530

    Add Nav buttons

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -19,6 +20,73 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.blue,
         shadowColor: Colors.lightBlue,
         elevation: 10,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      drawer: Drawer(
+        child: Center(
+          child: AnimatedTextKit(
+            animatedTexts: [
+              WavyAnimatedText(
+                'Made by Santosh',
+                textStyle: const TextStyle(fontSize: 25),
+              ),
+            ],
+            isRepeatingAnimation: true,
+          ),
+        ),
+      ),
+      body: Center(
+        child: SizedBox(
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.green),
+                    padding: WidgetStatePropertyAll(
+                        EdgeInsets.symmetric(vertical: 30))),
+                child: const Text(
+                  'Card Revealer',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.orange),
+                    padding: WidgetStatePropertyAll(
+                        EdgeInsets.symmetric(vertical: 30))),
+                child: const Text(
+                  'Card Swapper',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.purple),
+                    padding: WidgetStatePropertyAll(
+                        EdgeInsets.symmetric(vertical: 30))),
+                child: const Text(
+                  'Card Predictor',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
