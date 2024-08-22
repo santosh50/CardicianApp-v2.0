5568fbea1dc51aa0949da739a4254ea6c7d69500
Santosh Vasisht <svasisht@redhat.com>
2024-08-23 01:48:16 +0530

    Remove commented code

--- lib/pages/card_swapper.dart
+++ lib/pages/card_swapper.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:playing_cards/playing_cards.dart';
-import 'package:shake/shake.dart';
 
 import 'package:cardician_app_v2/card_class.dart';
-import 'dart:math';
 
 class CardSwapper extends StatefulWidget {
   const CardSwapper({super.key});
@@ -71,60 +69,6 @@ class _CardSwapperState extends State<CardSwapper> {
       ),
     );
   }
-
-  // void inputCard(MagicCard card) {
-  //   Suit inputSuit;
-  //   CardValue inputValue;
-
-  //   showDialog(
-  //     context: context,
-  //     barrierDismissible: false,
-  //     builder: (context) {
-  //       return isSuitSelected ? Container() : Container();
-  //     },
-  //   );
-  //   // builder:  (context) => Dialog(
-  //   //   child: GridView.count(
-  //   //     crossAxisCount: 4,
-  //   //     shrinkWrap: true,
-  //   //     physics: const NeverScrollableScrollPhysics(),
-  //   //     children: List.generate(
-  //   //         13,
-  //   //         (index) => TextButton(
-  //   //               child: Text(
-  //   //                 '${valueMap[valList[index]]}',
-  //   //                 style: const TextStyle(
-  //   //                     color: Colors.black,
-  //   //                     fontSize: 36,
-  //   //                     fontFamily: 'Georgia'),
-  //   //               ),
-  //   //               onPressed: () {
-  //   //                 inputValue = valList[index];
-  //   //                 // Navigator.of(context).pop();
-  //   //               },
-  //   //             )),
-  //   //   ),
-  //   // ),
-
-  // showDialog(
-  //   context: context,
-  //   builder: (context) => Dialog(
-  //     child: GridView.count(
-  //       crossAxisCount: 2,
-  //       shrinkWrap: true,
-  //       physics: const NeverScrollableScrollPhysics(),
-  //       children: List.generate(
-  //         4,
-  //         (index) => IconButton(
-  //             onPressed: () {
-  //               inputSuit = suitList[index];
-  //             },
-  //             icon: Image.asset('images/${suitMap[suitList[index]]}.png')),
-  //       ),
-  //     ),
-  //   ),
-  // );
-  // }
 
   @override
   Widget build(BuildContext context) {
