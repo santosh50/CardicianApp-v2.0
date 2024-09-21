2ca4f5d14f958540640f2a6d615a54ff5064dd00
Santosh Vasisht <svasisht@redhat.com>
2024-09-21 21:22:50 +0530

    Create CardGenerator() class

---
+++ lib/pages/card_generator.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:playing_cards/playing_cards.dart';
+
+import 'package:cardician_app_v2/card_class.dart';
+import 'package:cardician_app_v2/custom_playing_card.dart';
+
+class CardGenerator extends StatefulWidget {
+  const CardGenerator({super.key});
+
+  @override
+  State<CardGenerator> createState() => _CardGeneratorState();
+}
+
+class _CardGeneratorState extends State<CardGenerator> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
